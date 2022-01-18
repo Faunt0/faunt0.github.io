58f7b36e3f7af0d160b9bba4e6ad72eee0281b49
Faunt0 <kllex028@gmail.com>
2022-01-18 21:45:17 +0100

    added a total function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var price;
+var price, eth, eur, gaseth, gaseur;
 window.onload = function() {
 	const input1el = document.getElementById("firstin");
 	const input2el = document.getElementById("secondin");
@@ -8,18 +8,27 @@ window.onload = function() {
 	input1el.addEventListener("input", change);
 	input2el.addEventListener("input", change);
 	gweiin.addEventListener("input", calcgas);
+
 	document.getElementById("select1").value = "eth";
 	document.getElementById("select2").value = "eur";
-	// switchinputs();
+	document.getElementById("gwei").value = 100;
+
+	input1el.value = 1;
+
+	eth = 1;
+	// calcgas();
 
 	fetch('https://api.cryptonator.com/api/ticker/eth-eur').then(response =>{
     	return response.json();
 	}).then(data =>{
 		price = data.ticker.price;
-		input1el['placeholder'] = "1";
+		eur = price;
+		input1el['placeholder'] = eth;
 		input2el['placeholder'] = price;
-		console.log(price);
+
+
 		calcgas();
+		total();
 	})
 }
 
@@ -28,9 +37,11 @@ function ethupdate() {
     	return response.json();
 	}).then(data =>{
 		price = data.ticker.price;
-		console.log(price);
+
+		// console.log(price);
+
 		calcgas();
-		// change();
+		change();
 	})
 }
 setInterval(ethupdate, 30000);
@@ -39,32 +50,43 @@ function change (e) {
 	console.log("were changing");
 
 	if (e.target == document.getElementById("firstin")) {
-		document.getElementById('secondin').value = (e.target.value * price).toFixed(2);
+		eur = (e.target.value * price).toFixed(2);
+		document.getElementById('secondin').value = eur;
+
 	}
 	else {
-		document.getElementById('firstin').value = e.target.value / price;
+		eth = e.target.value / price;
+		document.getElementById('firstin').value = eur;
 	}
+
+	total();
 }
 
 function calcgas() {
 	console.log("we out here calcing gas");
 
     var gwei = Number(document.getElementById('gwei').value);
-	var eth = (gwei * 200000 / (10**9));
+	var gas = (gwei * 200000 / (10**9));
+	gaseur = (gas * price).toFixed(2);
+	gaseth = gas;
 
-	document.getElementById("gasprice").innerHTML = "&euro; " + (eth * Number(price)).toFixed(2);
-	document.getElementById("gaspriceETH").innerHTML = "Ξ " + eth;
+	document.getElementById("gasprice").innerHTML = "&euro; " + gaseur;
+	document.getElementById("gaspriceETH").innerHTML = "Ξ " + gas;
 }
 
-//doesnt work
 function switchinputs() {
 	var input2 = document.getElementById("secondin").value;
-	// console.log("were switching");
-	// console.log(input2)
 	document.getElementById("secondin").value = document.getElementById("firstin").value;
 	document.getElementById("firstin").value = input2;
 
 	var select1 = document.getElementById("select1").value
 	document.getElementById("select1").value = document.getElementById("select2").value;
 	document.getElementById("select2").value = select1;
+}
+
+function total() {
+	
+	document.getElementById("totaleur").innerHTML = "&euro; " + (Number(eur) + Number(gaseur)).toFixed(2);
+	document.getElementById("totaleth").innerHTML = "Ξ " + (Number(gaseth) + Number(eth));
+	
 }
